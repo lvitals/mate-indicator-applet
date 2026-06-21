3ab3c87356d3eecc12eccece552dd28f0d10d2ea
Leandro Vital <leandro.vital@yahoo.com.br>
2026-06-21 11:00:17 -0300

    build: add Meson targets for indicator applets

---
+++ mate-panel-applet-gsettings.h
@@ -0,0 +1,6 @@
+#ifndef MATE_INDICATOR_APPLET_COMPAT_MATE_PANEL_APPLET_GSETTINGS_H
+#define MATE_INDICATOR_APPLET_COMPAT_MATE_PANEL_APPLET_GSETTINGS_H
+
+#include <libmate-panel-applet/mate-panel-applet-gsettings.h>
+
+#endif
---
+++ mate-panel-applet.h
@@ -0,0 +1,6 @@
+#ifndef MATE_INDICATOR_APPLET_COMPAT_MATE_PANEL_APPLET_H
+#define MATE_INDICATOR_APPLET_COMPAT_MATE_PANEL_APPLET_H
+
+#include <libmate-panel-applet/mate-panel-applet.h>
+
+#endif
