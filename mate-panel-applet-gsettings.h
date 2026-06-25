00fc857e7318111bea0d648f16495107c0c58085
Leandro Vital <leandro.vital@yahoo.com.br>
2026-06-25 12:19:05 -0300

    fix: add fallback include mechanism for mate-panel-applet headers

--- mate-panel-applet-gsettings.h
+++ mate-panel-applet-gsettings.h
@@ -1,6 +1,10 @@
 #ifndef MATE_INDICATOR_APPLET_COMPAT_MATE_PANEL_APPLET_GSETTINGS_H
 #define MATE_INDICATOR_APPLET_COMPAT_MATE_PANEL_APPLET_GSETTINGS_H
 
+#if defined(__has_include) && __has_include(<libmate-panel-applet/mate-panel-applet-gsettings.h>)
 #include <libmate-panel-applet/mate-panel-applet-gsettings.h>
+#else
+#include_next <mate-panel-applet-gsettings.h>
+#endif
 
 #endif
--- mate-panel-applet.h
+++ mate-panel-applet.h
@@ -1,6 +1,10 @@
 #ifndef MATE_INDICATOR_APPLET_COMPAT_MATE_PANEL_APPLET_H
 #define MATE_INDICATOR_APPLET_COMPAT_MATE_PANEL_APPLET_H
 
+#if defined(__has_include) && __has_include(<libmate-panel-applet/mate-panel-applet.h>)
 #include <libmate-panel-applet/mate-panel-applet.h>
+#else
+#include_next <mate-panel-applet.h>
+#endif
 
 #endif
